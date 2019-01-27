0909692efcdda85c48594c3966106e34c48acefd
adammet <adam13met@gmail.com>
2019-01-26 19:20:55 -0800

    Trying Organizations Page

--- react-app/src/components/main.js
+++ react-app/src/components/main.js
@@ -3,16 +3,15 @@ import React from 'react';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './homepage';
 import MainPage from './mainpage';
+import OrganizationsPage from "./organizationspage";
 
 const Main = () => (
 	<HashRouter>
 			<Switch>
-				<Route exact path="/" component={HomePage} />
+				<Route exact path="/" component={OrganizationsPage} />
 				<Route exact path="/main" component={MainPage} />
 			</Switch>
 	</HashRouter>	
-	
-	
 )
 
 export default Main;
---
+++ react-app/src/components/organizationspage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class OrganizationsPage extends Component {
+
+    render() {
+
+        return(
+        <h1>Hello World</h1>
+        )
+    }
+}
+
+export default OrganizationsPage;
