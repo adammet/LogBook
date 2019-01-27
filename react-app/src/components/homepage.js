d473cb48abcd93d92f5865e0555e58e6cc222c7f
edmundohh <1edmundoh@gmail.com>
2019-01-26 16:43:39 -0800

    login page first commit

---
+++ react-app/src/components/homepage.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+
+
+class HomePage extends Component {
+
+	render() {
+		
+		return(
+
+  <header>
+   
+  	<div className="home-background">
+  	<div className='fade-in'>
+     
+     Log In   
+ 	</div> 
+ 	</div>
+ 	
+ </header>
+  
+		)
+	}
+}
+
+export default HomePage;
