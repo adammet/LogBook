7cea2c1ee67568db92f09f01841ee1b3240d8ebc
Morgan Law <morgankylaw@gmail.com>
2019-01-26 16:00:34 -0800

    getUserInfo

--- server/src/LogBookAPI.js
+++ server/src/LogBookAPI.js
@@ -1,6 +1,11 @@
 class LogBookAPI {
+
 	static async getUserInfo({user_id}) {
-		//let (success, reason, name, bio, preferences) = 
-		return 0;
+		let (success, reason, name, email, teams, organizations, tasks) = await UsersManager.getUserInfo({user_id});
+		let code = success ? 200 : 404;
+		let response = success ? {name, email, teams, organizations, tasks} : reason; 
+		
+		return {response, code};
 	}
+
 }
