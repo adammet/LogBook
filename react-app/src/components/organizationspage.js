0ca1ddc9d773cb42268b269f848940702474da08
adammet <adam13met@gmail.com>
2019-01-27 03:30:41 -0800

    Getting rid of warnings and error checking

--- react-app/src/components/organizationspage.js
+++ react-app/src/components/organizationspage.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 
 var org_array = ['Edmund', 'Adam'];
-var counter = 0;
 var num_of_orgs = org_array.length;
-var next_org;
 
 function generatebuttons() {
-    var arr = []
+    var arr = [];
     for (var i = 0; i < num_of_orgs; i++) {
         arr.push(i)
     }
@@ -14,7 +12,7 @@ function generatebuttons() {
         return generatenextbutton(i)
     })
 }
-
+//
 function generatenextbutton(counter){
     return (<div> <div className="divider"/> <button className='organization_button'> {org_array[counter]} </button> </div>);
 }
