08ad7b3a3656a6fd4057b0a648a7d7ca773c3958
Morgan Law <morgankylaw@gmail.com>
2019-01-26 16:24:32 -0800

    updates to getuserinfo

---
+++ server/src/UsersManager.js
@@ -0,0 +1,49 @@
+var mysql = require('mysql');
+
+class UsersManager {
+
+	static async getUserInfo({email}) {
+		let connection;
+		let reason;
+		let success = false;
+		let name;
+		let teams;
+		let organizations;
+		let tasks;
+
+		try {
+			var con = mysql.createConnection({
+				host: "localhost",
+				user: "admin",
+				password: "admin"
+			});
+
+			con.connect(function(err) {
+				if (err) throw err;
+				console.log("Connection successful");
+			});
+
+			var sql = "SELECT * FROM users WHERE email = " + email;
+
+			con.query(sql, function (err, result) {
+				if (err) throw err;
+				return result;
+			});
+
+		} catch (err) {
+			success = false;
+			reason = err;
+			console.log(err);
+		} finally {
+			if (connection) {
+				try {
+					connection.close();
+				} catch (err) {
+					console.error(err);
+				}
+			}
+		}
+		console.log(success);
+		return {success, reason, name, email, teams, organizations, tasks};
+	}
+}
